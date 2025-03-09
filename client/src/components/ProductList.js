fd77292cdeecea9ab9f85cad891a19f14cab129b
Mal <anityakumala@gmail.com>
2025-03-09 13:43:03 +0700

    Create ProductList.js

---
+++ client/src/components/ProductList.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await axios.get('http://localhost:5000/api/products');
+      setProducts(response.data);
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <div>
+      <h2>Products</h2>
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>
+            <h3>{product.name}</h3>
+            <p>{product.description}</p>
+            <p>Price: ${product.price}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ProductList;
